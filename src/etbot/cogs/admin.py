01a9821a617b32e25c4b846bcdc27e0961b5d86e
BrokenButler <castellobutler@gmail.com>
2022-02-22 00:36:07 +0100

    cogs can now be loaded/unloaded/reloaded on runtime resolves #5
    Signed-off-by: BrokenButler <castellobutler@gmail.com>
    
    Took 39 minutes

---
+++ src/etbot/cogs/admin.py
@@ -0,0 +1,54 @@
+from disnake.ext import commands
+from disnake.ext.commands import ExtensionNotFound, ExtensionAlreadyLoaded, ExtensionFailed, NoEntryPointError, \
+    ExtensionNotLoaded
+
+
+def setup(bot: commands.Bot) -> None:
+    bot.add_cog(Admin(bot))
+    print("Loaded Admin Cog.")
+
+
+class Admin(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @commands.command(name="cogs", aliases=["Cogs"],
+                      brief="Replies with a list of all the loaded cogs.")
+    @commands.has_guild_permissions(administrator=True)
+    async def cogs(self, ctx: commands.Context):
+        cogs: str = str(list(self.bot.cogs.keys()))
+        await ctx.send(cogs)
+
+    @commands.command(name="load", aliases=["Load"],
+                      brief="Loads a cog.")
+    @commands.has_guild_permissions(administrator=True)
+    async def load(self, ctx: commands.Context, cog: str):
+        try:
+            self.bot.load_extension(cog)
+        except (ExtensionNotFound, ExtensionAlreadyLoaded, NoEntryPointError, ExtensionFailed) as e:
+            await ctx.send(e)
+            return
+        await ctx.send("Cog loaded.")
+
+    @commands.command(name="unload", aliases=["Unload"],
+                      brief="Unloads a cog.")
+    @commands.has_guild_permissions(administrator=True)
+    async def unload(self, ctx: commands.Context, cog: str):
+        try:
+            self.bot.unload_extension(cog)
+        except (ExtensionNotFound, ExtensionNotLoaded) as e:
+            await ctx.send(e)
+            return
+        await ctx.send("Cog unloaded.")
+
+    @commands.command(name="reload", aliases=["Reload"],
+                      brief="RelLoads a cog.")
+    @commands.has_guild_permissions(administrator=True)
+    async def reload(self, ctx: commands.Context, cog: str):
+        try:
+            self.bot.unload_extension(cog)
+            self.bot.load_extension(cog)
+        except (ExtensionNotFound, ExtensionNotLoaded, ExtensionAlreadyLoaded, NoEntryPointError, ExtensionFailed) as e:
+            await ctx.send(e)
+            return
+        await ctx.send("Cog reloaded.")
