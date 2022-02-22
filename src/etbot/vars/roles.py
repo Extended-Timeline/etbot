0508a2b9e4bdf801a26ba35472277a183dadb729
BrokenButler <castellobutler@gmail.com>
2022-02-22 14:05:49 +0100

    added staff roles to roles.py they can be accessed individually, or as a list
    Signed-off-by: BrokenButler <castellobutler@gmail.com>

--- src/etbot/vars/roles.py
+++ src/etbot/vars/roles.py
@@ -1,15 +1,29 @@
 import sys
+from typing import List
 
+from disnake import Role
 from disnake.ext import commands
 
 this = sys.modules[__name__]
 
-senator = None
+senator: Role | None = None
+emperor: Role | None = None
+viceroy: Role | None = None
+palatine: Role | None = None
+
+# not really safe since it could be called before any of the values are assigned
+staff_roles: List[Role] = [emperor, viceroy, palatine]
 
 
 def initialize_testing_roles(bot: commands.Bot):
     this.senator = bot.get_guild(867738868181368852).get_role(867863600973742121)
+    this.emperor = bot.get_guild(867738868181368852).get_role(942901866021408848)
+    this.viceroy = bot.get_guild(867738868181368852).get_role(945664683615092756)
+    this.palatine = bot.get_guild(867738868181368852).get_role(945664760869949490)
 
 
 def initialize_roles(bot: commands.Bot):
     this.senator = bot.get_guild(485360396715425792).get_role(691576535781605417)
+    this.emperor = bot.get_guild(485360396715425792).get_role(485512381238083585)
+    this.viceroy = bot.get_guild(485360396715425792).get_role(485510055853293579)
+    this.palatine = bot.get_guild(485360396715425792).get_role(485515994492698625)
