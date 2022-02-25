af8223df7be8973369a203b8510a86832e65ef5b
BrokenButler <castellobutler@gmail.com>
2022-02-25 20:08:09 +0100

    Added typing to utils.py
    Signed-off-by: BrokenButler <castellobutler@gmail.com>

--- src/etbot/utils.py
+++ src/etbot/utils.py
@@ -1,7 +1,10 @@
+from disnake import Message
+from disnake.ext.commands import Context
+
 from src.etbot import main
 
 
-def is_me(ctx): return ctx.author == main.bot.user
+def is_me(ctx: Context | Message): return ctx.author == main.bot.user
 
 
-def is_not_me(ctx): return not ctx.author == main.bot.user
+def is_not_me(ctx: Context | Message): return not ctx.author == main.bot.user
