8a7496835b9b41396a884e790a1e4514e9295e40
BrokenButler <castellobutler@gmail.com>
2022-03-01 00:11:46 +0100

    added user_is_me and user_is_not_me functions
    Signed-off-by: BrokenButler <castellobutler@gmail.com>

--- src/etbot/utils.py
+++ src/etbot/utils.py
@@ -1,4 +1,5 @@
 from disnake import Message
+from disnake.abc import User
 from disnake.ext.commands import Context
 
 import main
@@ -7,4 +8,10 @@ import main
 def is_me(ctx: Context | Message): return ctx.author == main.bot.user
 
 
+def user_is_me(usr: User): return usr == main.bot.user
+
+
 def is_not_me(ctx: Context | Message): return not ctx.author == main.bot.user
+
+
+def user_is_not_me(usr: User): return not usr == main.bot.user
