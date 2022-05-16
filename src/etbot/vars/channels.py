07527987c657642bc1a075cc36cd5fb6d0e01171
BrokenButler <castellobutler@gmail.com>
2022-05-16 16:28:09 +0200

    Added #bot-log to channels.py
    Signed-off-by: BrokenButler <castellobutler@gmail.com>

--- src/etbot/vars/channels.py
+++ src/etbot/vars/channels.py
@@ -15,6 +15,8 @@ _memes_id: int | None = None
 _passed_bills_id: int | None = None
 _religious_memes_id: int | None = None
 _out_of_context_screenshots_id: int | None = None
+_bot_log_id: int | None = None
+
 
 def get_senate() -> GuildChannel | Thread | PrivateChannel:
     channel = _bot.get_channel(_senate_id)
@@ -72,6 +74,13 @@ def get_out_of_context_screenshots() -> GuildChannel | Thread | PrivateChannel:
     return channel
 
 
+def get_bot_log() -> GuildChannel | Thread | PrivateChannel:
+    channel = _bot.get_channel(_bot_log_id)
+    if channel is None:
+        raise Exception("#bot-log not found")
+    return channel
+
+
 # init to set all the values at the start of the bot
 def initialize_testing_channels(bot: commands.Bot):
     this._bot = bot
@@ -83,6 +92,7 @@ def initialize_testing_channels(bot: commands.Bot):
     this._passed_bills_id = 943157076648153089
     this._religious_memes_id = 971105466295935006
     this._out_of_context_screenshots_id = 974068964156125296
+    this._bot_log_id = 975726700467990528
 
 
 def initialize_channels(bot: commands.Bot):
@@ -95,3 +105,4 @@ def initialize_channels(bot: commands.Bot):
     this._passed_bills_id = 749320980354170972
     this._religious_memes_id = 969724432283234384
     this._out_of_context_screenshots_id = 874438552056913970
+    this._bot_log_id = 485542137660964873
